e5a974125fd73ac51c0d60bcea75d086cd3f95b8
danixuz <danixuz@icloud.com>
2022-07-06 05:27:25 +0300

    Initial Commit

---
+++ NativePhotoPickerTest/NativePhotoPickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativePhotoPickerTestApp.swift
+//  NativePhotoPickerTest
+//
+//  Created by Daniel Spalek on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct NativePhotoPickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
